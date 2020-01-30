886cb67d6c7d15c9c76fdacb3f9715dc2492e254
Brittanie <bs009@live.com>
2020-01-30 09:56:27 -0500

    added client files

---
+++ project/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
