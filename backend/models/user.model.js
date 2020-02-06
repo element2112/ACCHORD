2d460c26aee04e616d22e3e8f733e923bd98a6d1
Rebecca Englehart <renglehart7@knights.ucf.edu>
2020-02-06 17:13:03 -0500

    user schema update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
+    email: {
         type: String,
         required: true,
         unique: true,
