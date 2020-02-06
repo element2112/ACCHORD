863d298579831b018017a2c71420dc3087810bec
Rebecca Englehart <renglehart7@knights.ucf.edu>
2020-02-06 17:17:48 -0500

    add user schema

---
+++ api/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: false,
+        minlength: 5
+    }, 
+    firstName: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
