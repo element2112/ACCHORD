77ba9243795841a18d11d55aa70f6fbb518a5cde
Brittanie <bs009@live.com>
2020-02-09 12:33:29 -0500

    heroku test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const path = require('path');
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const connection = mongoose.connection;
