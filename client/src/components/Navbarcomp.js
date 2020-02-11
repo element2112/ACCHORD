8c38ecb57ee28a7ec98e009987a3b44ec06985d4
Brittanie <bs009@live.com>
2020-02-10 19:19:20 -0500

    removed searchbar

--- client/src/components/Navbarcomp.js
+++ client/src/components/Navbarcomp.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Navbar, FormControl, Button, Form, Nav } from 'react-bootstrap'
-import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
 
 function Navbarcomp() {
 
   // let isBye = false;
 
-  // const logout = () => {
-  // localStorage.clear();
-  // isBye = true;
-  // }
+  const clear = () => {
+    console.log("here")
+    localStorage.clear();
+    console.log("cleared");
+  }
 
   // if (isBye)
   // {
@@ -22,16 +23,16 @@ function Navbarcomp() {
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand href="#home">My Acchord Home</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="/dashboard">Home</Nav.Link>
           <Nav.Link href="#features">Playlists</Nav.Link>
           <Nav.Link href="#pricing">My Friends</Nav.Link>
           <Nav.Link href="#My Account">My Account</Nav.Link>
-          <Nav.Link href="/">Logout</Nav.Link>
+          <Nav.Link href="/" onClick={clear}>Logout</Nav.Link>
         </Nav>
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-light">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar>
     </>
   )
