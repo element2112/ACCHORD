842df4f986b25451c2e01a0f598ed6fda8c8ee71
homshnogen <homshnogen@gmail.com>
2020-02-12 13:45:40 -0500

    Modified Login.js. Implemented changes to react state, form submit in progress.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,36 +7,56 @@ import { Redirect, Link } from 'react-router-dom';
 // import CenterView from './CenterView'
 
 export class Login extends Component {
-
+  
   state = {
     email: "",
     password: ""
   }
-
-  handleClick(e) {
+  
+  submitForm = (e) => {
+    console.log(this.state);
     e.preventDefault();
+  }
+  
+  update = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    let errorstate = '';
+    switch(name){
+    case "email":
+      // validate the email to prevent some attacks
+      break;
+    case "password":
+      //validate the password to prevent some attacks
+      if (value.length > 20) {
+        value = value.substr(20);
+      }
+      break;
+    }
+    console.log(value);
+    this.setState({[name]: value});
+    console.log(this.state);
+  }
+  
+  handleClick(e) {
+    //e.preventDefault();
   }
 
   render() {
     return (
       <Form style={{
         position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-      }}>
+        transform: 'translate(-50%, -50%)', 
+      }} onSubmit={this.submitForm}>
         <h1>Login to Acchord</h1>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-          </Form.Text>
+          <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.update} />
         </Form.Group>
       
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
+          <Form.Control type="password" placeholder="Password" name="password" onChange={this.update} />
         </Form.Group>
         <Button variant="primary" type="submit" onClick={this.handleClick}>
           Login
