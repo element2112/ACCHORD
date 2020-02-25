da3c20ea34272874374c8572514515276e1ec5c4
Brittanie <bs009@live.com>
2020-02-24 20:04:09 -0500

    added spinner to fix embedded playlist rendering before track has been added

--- client/src/components/LoginSpot.js
+++ client/src/components/LoginSpot.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 import queryString from 'query-string'
 import Playlist from './Playlist';
+import { Spinner } from 'react-bootstrap'
 
 export default class LoginSpot extends Component {
 
@@ -15,20 +16,15 @@ export default class LoginSpot extends Component {
       link: '',
       playlistID: '',
       trackID: '',
-      track: 'alison',
-      artist: 'elvis costello',
+      track: 'yellow',
+      artist: 'coldplay',
       market: 'US',
-      uri: ''
+      uri: '',
+      loading: false
     }
 
     this.handlePlaylists = this.handlePlaylists.bind(this)
   }
-
-  // mounting component
-  // async componentDidMount() {
-  //   console.log('COMPONENT MOUNTED')
-    
-  // }
 
   // all playlist fetching
   async handlePlaylists() {
@@ -36,10 +32,11 @@ export default class LoginSpot extends Component {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
-    let res = await fetch('https://api.spotify.com/v1/me', {
+    await fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken}
     }).then((res) => res.json())
       .then((data) => this.setState({ username: data.display_name, spotifyToken: accessToken }))
+      .then(() => this.setState({ loading: true }))
       .catch((err) => alert(`Cannot login due to ${err}`))
     
     // creating playlist
@@ -61,7 +58,6 @@ export default class LoginSpot extends Component {
     }).then((res) => res.json())
       .then(data => this.setState({ trackID: data.tracks.items[0].id, uri: data.tracks.items[0].uri }))
       .catch((err) => alert('Cant create a playlist at this time. Make sure you are logged into spotify: ' + err))
-      .then(() => console.log(`artist: ${this.state.trackID}`))
 
     // adding track to playlist
     await fetch(`https://api.spotify.com/v1/users/${this.state.username}/playlists/${this.state.playlistID}/tracks`, {
@@ -73,19 +69,35 @@ export default class LoginSpot extends Component {
     }).then((res) => res.json())
       .then((data) => this.setState({ playlistID: data.snapshot_id}))
       .then(() => console.log('FETCHING TRACK'))
+      .then(() => this.setState({ loading: false }))
       .catch((err) => alert('Cant create a playlist at this time. Make sure you are logged into spotify: ' + err))
     
   }
 
   // render
   render() {
-    const { link } = this.state;
-    return (
-      <div>
-        <Playlist playlists={link} />
-        <Button id="login" onClick={() => window.location = 'http://localhost:8888/spotifylogin'}>Login with Spotify</Button>
-        <Button data-testid="handleList" id="playlists" onClick={this.handlePlaylists}>New Playlist</Button>
-      </div>
-    )
+    if (!this.state.loading)
+    {
+      return ( 
+        <div>
+          <Playlist playlists={this.state.link} /> 
+          <Button id="login" onClick={() => window.location = 'http://localhost:8888/spotifylogin'}>Login with Spotify</Button>
+          <Button data-testid="handleList" id="playlists" onClick={this.handlePlaylists}>New Playlist</Button>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <Spinner animation="border" role="status" style={{ position:"fixed", top:"20%", left:"50%" }}>
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+          <Playlist /> 
+          <Button id="login" onClick={() => window.location = 'http://localhost:8888/spotifylogin'}>Login with Spotify</Button>
+          <Button data-testid="handleList" id="playlists" onClick={this.handlePlaylists}>New Playlist</Button>
+        </div>
+      )
+    }
+    
   }
 }
