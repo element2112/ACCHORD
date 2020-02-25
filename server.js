1881aeb68be253d5066939cd9ce82a01806d7210
Brittanie <bs009@live.com>
2020-02-25 11:16:38 -0500

    reverted back to previous commit and made heroku changes

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const users = require('./api/users');
 
 // const spotify = require('./api/spotify');
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') { 
+  require('dotenv').config() 
+}
 
 const app = express();
 const port = process.env.PORT || 4000;
