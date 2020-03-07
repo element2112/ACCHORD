a3f795ba59ae78a3bbf20012fdeba21f783d3890
Brittanie <bs009@live.com>
2020-03-07 10:31:41 -0500

    completed api call for playlist

--- client/src/services/send_playlist.js
+++ client/src/services/send_playlist.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-function send_playlist(progression ) {
-  const res = await fetch('/api/something', {
-    method: 'POST' ,
-    headers: 'Content-Type': 'application/json',
+function send_playlist(progression) {
+  const res = await fetch('http://localhost:4000/api/users/songs', {
+    method: 'GET' ,
+    headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
-
-    })
+      "pos1": `${progression.chord1}`,
+      "pos2": `${progression.chord2}`,
+      "pos3": `${progression.chord3}`,
+      "pos4": `${progression.chord4}`
+    }),
   })
 
   return res.json();
