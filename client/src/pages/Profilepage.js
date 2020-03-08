f99d6297fd1056127cd97115388a1d8fe4b193b3
homshnogen <homshnogen@gmail.com>
2020-03-08 13:00:22 -0400

    Added the profile page and components!

--- client/src/pages/Profilepage.js
+++ client/src/pages/Profilepage.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Navbarcomp from '../components/Navbarcomp'
 import { Container } from 'react-bootstrap'
 import Profile from '../components/Profile';
-import Chordcomp from '../components/Chordcomp';
-import LoginSpot from '../components/LoginSpot';
 
 export class Profilepage extends Component {
   render() {
@@ -11,13 +9,7 @@ export class Profilepage extends Component {
     return (
       <>
         <Navbarcomp />
-        <Container>
-          <LoginSpot />
-        </Container>
-        <Container>
           <Profile />
-        </Container>
-        <Chordcomp />
       </>
     )
   }
