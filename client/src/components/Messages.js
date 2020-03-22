888ee729c162663eb725fbc20b681e8f44bd3c23
Alexandra Arriola <alexandra.arriola@knights.ucf.edu>
2020-03-22 11:54:11 -0400

    added form for messages

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import { Card, Col } from 'react-bootstrap'
 // import Navbarcomp from './Navbarcomp'
-import { Redirect } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const headers = {
+  'Content-Type': 'application/json'
+}
 
 export class Messages extends Component {
 
@@ -13,7 +18,8 @@ export class Messages extends Component {
       lastName: '',
       email: '',
       password: '',
-      authenticated: false
+      authenticated: false,
+      messages: ''
     }
   }
 
@@ -29,9 +35,36 @@ export class Messages extends Component {
         lastName: localStorage.getItem('lastName'),
         email: localStorage.getItem('email'),
         password: localStorage.getItem('password'),
+        messages: localStorage.getItem('messages'),
         authenticated: true
       })
     }
+  }
+
+  handleCommentSubmission = () => {
+    console.log("refreshing");
+    this.setState({refresh: true});
+  }
+
+  handleSubmit = (e) => {
+    console.log('leaving comment');
+    let state = this.state;
+    fetch("http://localhost:4000/api/users/updateuser",
+    {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(state)
+    })
+    .then(res => res.json())
+    .then(this.handleCommentSubmission);
+    e.preventDefault();
+  }
+
+  update = (e) => {
+    console.log("Changing state!");
+    let name = e.target.name;
+    let value = e.target.value;
+    this.setState({[name]: value});
   }
 
   render() {
@@ -42,13 +75,21 @@ export class Messages extends Component {
             <Card.Body>
               <Card.Title style={{color: "black"}}>{this.state.firstName}'s messages</Card.Title>
               <Card.Text style={{color: "black"}} >
-                This is a message
+                 <Form>
+                    <Form.Group controlId="formMessage">
+                      <Form.Label>Leave A Message!</Form.Label>
+                      <Form.Control type="messages" placeholder="Write your message here!" name="messages" onChange={this.update} />
+                    </Form.Group>
+                    <Button data-testid="submitButton" variant="primary" type="submit">
+                      Submit
+                    </Button>
+                  </Form>
               </Card.Text>
               <Card.Text style={{color: "black"}} >
-                This is a message
+                {this.state.messages}
               </Card.Text>
               <Card.Text style={{color: "black"}} >
-                This is a message
+                This is a default message.
               </Card.Text>
             </Card.Body>
           </Card>
