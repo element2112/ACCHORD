cd4c5b9d76ea6d7537f40acb9ce32c3e942b38e9
Brittanie <bs009@live.com>
2020-04-07 10:41:18 -0400

    changed songs route in frontend

--- client/src/services/send_playlist.js
+++ client/src/services/send_playlist.js
@@ -14,7 +14,7 @@ async function send_playlist(progression) {
 
   console.log('new: ' + new_array)
 
-  const res = await fetch('http://localhost:4000/api/users/songs', {
+  const res = await fetch('/api/users/songs', {
     method: 'POST' ,
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
