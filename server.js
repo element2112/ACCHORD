413a6933b3b1cdbe5ffad22a9a2c638c1bb3f64e
Brittanie <bs009@live.com>
2020-04-10 12:06:01 -0400

    heroku testing

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.get('/callback', function(req, res) {
       var access_token = body.access_token
       let uri = process.env.FRONTEND_URI || 'http://localhost:3000/dashboard'
       res.redirect(uri + '?access_token=' + access_token)
+      console.log('uri: ' + uri);
       console.log(`Access token: ${access_token}`)
     })
   })
